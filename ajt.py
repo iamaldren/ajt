ad5dfe03c72bcb9d37fb64917313aba2a8fbfec3
iamaldrew <aldrenbobiles@gmail.com>
2019-08-31 23:32:02 +0800

    Removed JSONBender, and created my own JSON transformation

--- ajt.py
+++ ajt.py
@@ -1,20 +1,41 @@
 import json
 import yaml
 
-from jsonbender import bend, S
-
 
 def main():
     with open("./schema/schema1.yml", 'r') as stream:
         data_loaded = yaml.safe_load(stream)
 
-        mapping = {}
-        for key in data_loaded['fields']:
-            mapping[key] = S(data_loaded['fields'][key])
-
-        source = {'pangalan': 'Aldren', 'apelyido': 'Bobiles'}
-        result = bend(mapping, source)
+        source = {
+            'pangalan': 'Aldren',
+            'apelyido': 'Bobiles',
+            'trabaho': {
+                'posisyon': 'Developer',
+                'industriya': 'IT',
+                'kakayahan': {
+                    'lengwahe': 'Python'
+                }
+            }
+        }
+        result = transform(source, data_loaded['fields'])
         print(json.dumps(result, indent=4, sort_keys=True))
+
+
+def transform(source, mapping):
+    data = {}
+
+    for key in mapping:
+        if key == 'array':
+            array_key = next(iter(mapping[key]))
+            array = mapping[key][array_key]
+            data[array_key] = transform(source[next(iter(array))], array)
+        else:
+            if type(mapping[key]) is dict:
+                data = transform(source, mapping[key])
+            else:
+                data[key] = source[mapping[key]]
+
+    return data
 
 
 if __name__ == "__main__":
