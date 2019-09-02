11d9827fd4fd408604736d90f4caf9ef11e8c83b
iamaldrew <aldrenbobiles@gmail.com>
2019-09-02 22:05:03 +0800

    Added handling for multiple schemal files handler

--- ajt.py
+++ ajt.py
@@ -1,24 +1,30 @@
 import json
+import os
 import yaml
 
 
 def main():
-    with open("./schema/schema1.yml", 'r') as stream:
-        data_loaded = yaml.safe_load(stream)
+    path = "./schema/"
 
-        source = {
-            'pangalan': 'Aldren',
-            'apelyido': 'Bobiles',
-            'trabaho': {
-                'posisyon': 'Developer',
-                'industriya': 'IT',
-                'kakayahan': {
-                    'Country_Code': 'Python'
-                }
-            }
-        }
-        result = transform(source, data_loaded['fields'])
-        print(json.dumps(result, indent=4, sort_keys=True))
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.yml' in file:
+                with open(os.path.join(r, file), 'r') as stream:
+                    data_loaded = yaml.safe_load(stream)
+
+                    source = {
+                        'pangalan': 'Aldren',
+                        'apelyido': 'Bobiles',
+                        'trabaho': {
+                            'posisyon': 'Developer',
+                            'industriya': 'IT',
+                            'kakayahan': {
+                                'lengwahe': 'Python'
+                            }
+                        }
+                    }
+                    result = transform(source, data_loaded['fields'])
+                    print(json.dumps(result, indent=4, sort_keys=True))
 
 
 def transform(source, mapping):
