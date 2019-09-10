91e997456139d0f0f9e663dd25e5a4af1a728f5d
iamaldrew <aldrenbobiles@gmail.com>
2019-09-10 08:31:19 +0800

    Restructuring beding logic

--- ajt.py
+++ ajt.py
@@ -1,26 +1,35 @@
 import json
 import yaml
 
+count = 0
+
 
 def transform(jsonschema, data):
     with open(jsonschema, 'r') as stream:
         schema_loaded = yaml.safe_load(stream)
 
-        result = doTransform(data, schema_loaded['fields'])
+        result = shift(data, schema_loaded['shift'])
         print(json.dumps(result, indent=4, sort_keys=True))
 
 
-def doTransform(sourcedata, mapping):
+def shift(sourcedata, mapping):
     data = {}
+    subdata = {}
+
+    global count
+    count += 1
+    print("count is ", count)
 
     for key in mapping:
-        if key == 'nested':
-            array_key = next(iter(mapping[key]))
-            array = mapping[key][array_key]
-            data[array_key] = doTransform(sourcedata[next(iter(array))], array)
-        elif type(mapping[key]) is dict:
-            data = doTransform(sourcedata, mapping[key])
+        if type(mapping[key]) is dict:
+            data = shift(sourcedata[key], mapping[key])
+        elif ":" in mapping[key]:
+            pair = mapping[key].split(":")
+            parent = pair[0]
+            child = pair[1]
+            subdata[child] = sourcedata[key]
+            data[parent] = subdata
         else:
-            data[key] = sourcedata[mapping[key]]
+            data[mapping[key]] = sourcedata[key]
 
     return data
